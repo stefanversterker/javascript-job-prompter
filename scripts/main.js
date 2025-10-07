d6b9b76a43f36f13d7d5e01a292a17b86f2c12ef
stefanversterker <stefanversterker@gmail.com>
2025-10-07 17:25:37 +0200

    opdracht-2

--- scripts/main.js
+++ scripts/main.js
@@ -72,3 +72,14 @@ const departments = {
 }
 
 console.log(departments);
+
+console.log("De afdeling Sales heeft " + departments.sales.numberOfEmployees + " medewerkers")
+
+console.log(" Marketing is een leuke afdeling om te werken. " + departments.marketing.description + ".")
+
+console.log("De afdeling Customer Service heeft " + departments["customer-service"].numberOfEmployees + " medewerkers")
+
+console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager. " + departments.sales.jobs[1].description)
+
+const userInput = prompt('Hoi! Hoe heet je?');
+console.log(userInput);
